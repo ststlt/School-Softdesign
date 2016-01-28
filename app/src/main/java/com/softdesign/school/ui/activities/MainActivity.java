6268bca461ae0cb7cca51f2a9896db1bab910657
Shevtsov Stas <shevtsov_stas@rambler.ru>
2016-01-29 00:08:13 +0400

    Созданы view в activity_main.xml (text, input - 2, checkbox, buttons - 3). Созданы пустые методы жизненного цикла activity.

---
+++ app/src/main/java/com/softdesign/school/ui/activities/MainActivity.java
@@ -0,0 +1,45 @@
+package com.softdesign.school.ui.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.softdesign.school.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+    }
+}
