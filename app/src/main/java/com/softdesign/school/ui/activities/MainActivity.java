a10240ff81111b97d7d6a1bf29929b9695b75d4b
Shevtsov Stas <shevtsov_stas@rambler.ru>
2016-02-02 20:35:19 +0400

    HomeTask 3: 1. Реализация Navigation Drawer с элементами меню.

--- app/src/main/java/com/softdesign/school/ui/activities/MainActivity.java
+++ app/src/main/java/com/softdesign/school/ui/activities/MainActivity.java
@@ -1,7 +1,11 @@
 package com.softdesign.school.ui.activities;
 
 import android.os.Build;
+import android.support.design.widget.NavigationView;
+import android.support.v4.app.Fragment;
 import android.support.v4.content.ContextCompat;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
@@ -11,10 +15,13 @@ import android.view.Window;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.EditText;
+import android.widget.FrameLayout;
 import android.widget.Toast;
 
 
 import com.softdesign.school.R;
+import com.softdesign.school.ui.fragments.ContactsFragment;
+import com.softdesign.school.ui.fragments.ProfileFragment;
 import com.softdesign.school.utils.Lg;
 
 /* Имплементация слушателя события Click по элементу View */
@@ -36,6 +43,12 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     private Integer mToolbarColor = R.color.colorPrimary;
     private Integer mStatusColor = R.color.colorPrimaryDark;
 
+    private DrawerLayout mNavigationDrawer; // Обертка меню навигации
+    private NavigationView mNavigationView; // Здесь подключается элементы меню drawer_menu.xml, включая navigation_header.xml
+
+    private Fragment mFragment;
+    private FrameLayout mFrameContainer;
+
     /* Статическая переменная, в которой хранится состояние скрытого/видимого второго input (см. outState) */
     private static final String VISIBLE_KEY = "visible";
     private static final String TOOLBAR_COLOR_KEY = "toolbar";
@@ -53,37 +66,83 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         /* Задаем свой заголовок в ActionBar |
         ToolBar - это тот же ActionBar, но представленный в виде view-элемента в xml
         (см. res/values/styles.xml - необходимо наследоваться от Theme.AppCompat.Light.NoActionBar (текущая тема Theme.AppCompat.Light.DarkActionBar)) */
-        setTitle("Lesson 2");
+        setTitle("Lesson 3");
 
         Lg.e(this.getLocalClassName(), "on create"); /* Сообщение в логгер о создании главного активити */
 
+        /* Используется в уроке #2 */
         /* Создание экземпляра объекта Checkbox для дальнейшей работы с ним | Приведение типов обязательно | Поиск по идентификатору в классе R */
-        mCheckBox = (CheckBox) findViewById(R.id.checkBox);
+//        mCheckBox = (CheckBox) findViewById(R.id.checkBox);
         /* Вешаем обработчик клика на созданный экземпляр чекбокса предварительно имплементировав листнер View.OnClickListener и создав метод onClick */
-        mCheckBox.setOnClickListener(this);
+//        mCheckBox.setOnClickListener(this);
 
-        mEditText = (EditText) findViewById(R.id.editText);
-        mEditText2 = (EditText) findViewById(R.id.editText2);
+//        mEditText = (EditText) findViewById(R.id.editText);
+//        mEditText2 = (EditText) findViewById(R.id.editText2);
 
-        mBtnBlue = (Button) findViewById(R.id.btn_blue);
-        mBtnBlue.setOnClickListener(this);
+//        mBtnBlue = (Button) findViewById(R.id.btn_blue);
+//        mBtnBlue.setOnClickListener(this);
 
-        mBtnGreen = (Button) findViewById(R.id.btn_green);
-        mBtnGreen.setOnClickListener(this);
+//        mBtnGreen = (Button) findViewById(R.id.btn_green);
+//        mBtnGreen.setOnClickListener(this);
 
-        mBtnRed = (Button) findViewById(R.id.btn_red);
-        mBtnRed.setOnClickListener(this);
+//        mBtnRed = (Button) findViewById(R.id.btn_red);
+//        mBtnRed.setOnClickListener(this);
+        /* Используется в уроке #2 */
 
         mToolbar = (Toolbar) findViewById(R.id.toolbar);
 
         mWindow = (Window) getWindow();
 
+        /* Меню навигации */
+        mNavigationDrawer = (DrawerLayout) findViewById(R.id.navigation_drawer);
+        mNavigationView = (NavigationView) findViewById(R.id.navigation_view);
+
         setupToolbar();
+        setupDrawer();
+
+        /* Проверка, запускается активити в первый раз или нет */
+        if (savedInstanceState != null) { // не первый раз
+/*            int visibleState = savedInstanceState.getBoolean(VISIBLE_KEY) ? View.VISIBLE : View.INVISIBLE;
+            mEditText.setVisibility(visibleState);*/
+
+        } else { // в первый раз, значит нужно загрузить первый фрагмент
+            getSupportFragmentManager().beginTransaction().replace(R.id.main_frame_container, new ProfileFragment()).commit(); // из activity_main.xml
+        }
+    }
+
+    /* Инициализация меню навигации */
+    private void setupDrawer() {
+        mNavigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+
+            @Override
+            public boolean onNavigationItemSelected(MenuItem item) {
+                switch (item.getItemId()) {
+                    case R.id.drawer_profile:
+                        mFragment = new ProfileFragment();
+                        mNavigationView.getMenu().findItem(R.id.drawer_profile).setChecked(true);
+                        Toast.makeText(MainActivity.this, item.getTitle().toString(), Toast.LENGTH_SHORT).show();
+                        break;
+                    case R.id.drawer_contacts:
+                        mFragment = new ContactsFragment();
+                        mNavigationView.getMenu().findItem(R.id.drawer_contacts).setChecked(true);
+                        Toast.makeText(MainActivity.this, item.getTitle().toString(), Toast.LENGTH_SHORT).show();
+                        break;
+                }
+
+                if (mFragment != null) {
+                    getSupportFragmentManager().beginTransaction().replace(R.id.main_frame_container, mFragment).addToBackStack(null).commit(); // из activity_main.xml
+                }
+
+                mNavigationDrawer.closeDrawers();
+                return false;
+            }
+
+        });
     }
 
 
     /* Инициализация тулбара после танцев со стилями */
-    private void setupToolbar () {
+    private void setupToolbar() {
         setSupportActionBar(mToolbar);
         android.support.v7.app.ActionBar actionBar = getSupportActionBar();
         /* Если экшнбар поддерживается, не нулл */
@@ -99,7 +158,10 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         if (item.getItemId() == android.R.id.home) {
-            Toast.makeText(this, "Menu click", Toast.LENGTH_SHORT).show();
+            /*Toast.makeText(this, "Menu click", Toast.LENGTH_SHORT).show();*/
+
+            /* Обработчик клика по сэндвичу */
+            mNavigationDrawer.openDrawer(GravityCompat.START); // Открытие меню навигации слева (START). Справа (END).
         }
         return super.onOptionsItemSelected(item);
     }
@@ -179,9 +241,9 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         super.onSaveInstanceState(outState);
         Lg.e(this.getLocalClassName(), "on Save Instance State");
         /* Сохранение в Bundle текущего состояния */
-        outState.putBoolean(VISIBLE_KEY, mEditText2.getVisibility() == View.VISIBLE);
+        /*outState.putBoolean(VISIBLE_KEY, mEditText2.getVisibility() == View.VISIBLE);
         outState.putInt(TOOLBAR_COLOR_KEY, mToolbarColor);
-        outState.putInt(STATUSBAR_COLOR_KEY, mStatusColor);
+        outState.putInt(STATUSBAR_COLOR_KEY, mStatusColor);*/
     }
 
     /* За счет этого метода происходит восстановление значений из Bundle для view-элементов, не имеющих идентификатор */
@@ -192,20 +254,20 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         int visibleState = savedInstanceState.getBoolean(VISIBLE_KEY) ? View.VISIBLE : View.INVISIBLE;
 
         /* Задаем второму input полученное состояние из Bundle */
-        mEditText2.setVisibility(visibleState);
+        /*mEditText2.setVisibility(visibleState);
 
         setmToolbarColor(savedInstanceState.getInt(TOOLBAR_COLOR_KEY));
 
-        setmStatusColor(savedInstanceState.getInt(STATUSBAR_COLOR_KEY));
+        setmStatusColor(savedInstanceState.getInt(STATUSBAR_COLOR_KEY));*/
 
     }
 
-    private void setmToolbarColor (Integer color) {
+    private void setmToolbarColor(Integer color) {
         mToolbar.setBackgroundColor(ContextCompat.getColor(this, color));
         mToolbarColor = color;
     }
 
-    private void setmStatusColor (Integer color) {
+    private void setmStatusColor(Integer color) {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
             Lg.e(this.getLocalClassName(), "on Set Status Bar Color: " + color);
             mWindow.setStatusBarColor(ContextCompat.getColor(this, color));
---
+++ app/src/main/java/com/softdesign/school/ui/fragments/ProfileFragment.java
@@ -0,0 +1,27 @@
+package com.softdesign.school.ui.fragments;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.softdesign.school.R;
+
+public class ProfileFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+
+        /* Надуваем из xml лэйаута view элемент */
+        View convertView = inflater.inflate(R.layout.fragment_profile, null, false);
+
+        /* Задаем в тулбаре выбранное в меню навигации наименование */
+        getActivity().setTitle(getResources().getString(R.string.drawer_profile));
+
+        return convertView;
+//        return super.onCreateView(inflater, container, savedInstanceState);
+    }
+}
