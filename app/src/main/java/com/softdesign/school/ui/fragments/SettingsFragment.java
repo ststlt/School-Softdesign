5f88e9e40a305983ba607108fb9f0f59a2e5fe14
Shevtsov Stas <shevtsov_stas@rambler.ru>
2016-02-16 22:47:49 +0400

    HomeTask 4: 1. Реализован CollapsingToolbar

--- app/src/main/java/com/softdesign/school/ui/fragments/SettingsFragment.java
+++ app/src/main/java/com/softdesign/school/ui/fragments/SettingsFragment.java
@@ -2,12 +2,16 @@ package com.softdesign.school.ui.fragments;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.design.widget.CoordinatorLayout;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.Fragment;
+import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.softdesign.school.R;
+import com.softdesign.school.ui.activities.MainActivity;
 
 public class SettingsFragment extends Fragment {
     @Nullable
@@ -19,7 +23,20 @@ public class SettingsFragment extends Fragment {
         /* Задаем в тулбаре выбранное в меню навигации наименование */
         getActivity().setTitle(getResources().getString(R.string.drawer_settings));
 
+        ( (MainActivity) getActivity() ).lockAppBar(true);
+
         return convertView;
 //        return super.onCreateView(inflater, container, savedInstanceState);
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        FloatingActionButton fab = (FloatingActionButton) getActivity().findViewById(R.id.fab); //инициализируем fab из активити
+        CoordinatorLayout.LayoutParams params = (CoordinatorLayout.LayoutParams) fab.getLayoutParams(); // получаем параметры Layout fab приведенные к родителю
+        params.setAnchorId(R.id.appbar_layout); //выставляем привязку якоря к appBarLayout
+        params.anchorGravity = Gravity.BOTTOM | Gravity.RIGHT; //выставляем anchorGravity
+        fab.setLayoutParams(params);
+        fab.setImageResource(R.drawable.ic_phone_24dp); // меняем иконку fab
     }
 }
--- app/src/main/java/com/softdesign/school/ui/fragments/TasksFragment.java
+++ app/src/main/java/com/softdesign/school/ui/fragments/TasksFragment.java
@@ -2,12 +2,16 @@ package com.softdesign.school.ui.fragments;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.design.widget.CoordinatorLayout;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.Fragment;
+import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.softdesign.school.R;
+import com.softdesign.school.ui.activities.MainActivity;
 
 public class TasksFragment extends Fragment {
     @Nullable
@@ -20,7 +24,20 @@ public class TasksFragment extends Fragment {
         /* Задаем в тулбаре выбранное в меню навигации наименование */
         getActivity().setTitle(getResources().getString(R.string.drawer_tasks));
 
+        ( (MainActivity) getActivity() ).lockAppBar(true);
+
         return convertView;
 //        return super.onCreateView(inflater, container, savedInstanceState);
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        FloatingActionButton fab = (FloatingActionButton) getActivity().findViewById(R.id.fab); //инициализируем fab из активити
+        CoordinatorLayout.LayoutParams params = (CoordinatorLayout.LayoutParams) fab.getLayoutParams(); // получаем параметры Layout fab приведенные к родителю
+        params.setAnchorId(R.id.appbar_layout); //выставляем привязку якоря к appBarLayout
+        params.anchorGravity = Gravity.BOTTOM | Gravity.RIGHT; //выставляем anchorGravity
+        fab.setLayoutParams(params);
+        fab.setImageResource(R.drawable.ic_phone_24dp); // меняем иконку fab
     }
 }
